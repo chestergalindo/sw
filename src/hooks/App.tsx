0e344ba4eb2a580cac87215bb6f7d40441f95dbb
César Galindo <33326122+chestergalindo@users.noreply.github.com>
2022-09-26 07:40:38 -0500

    :alien: feat (api): connect graphql

--- src/hooks/App.tsx
+++ src/hooks/App.tsx
@@ -1,14 +1,23 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { RouteList } from '../Routes/RouteList';
 import { AppProvider } from './context';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
 export const App = () => {
   const queryClient = new QueryClient();
+
+  const client = new ApolloClient({
+    uri: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <AppProvider>
-        <RouteList />
-      </AppProvider>
-    </QueryClientProvider>
+    <ApolloProvider client={client}>
+      <QueryClientProvider client={queryClient}>
+        <AppProvider>
+          <RouteList />
+        </AppProvider>
+      </QueryClientProvider>
+    </ApolloProvider>
   );
 };
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,3 +1,68 @@
-export const Home = () => {
-  return <p>hello world</p>;
+import { useQuery, gql } from '@apollo/client';
+
+const GET_LOCATIONS = gql`
+  query Query($after: String) {
+    allPeople(first: 10, after: $after) {
+      totalCount
+      pageInfo {
+        endCursor
+      }
+      people {
+        name
+        filmConnection {
+          films {
+            title
+            director
+            producers
+            releaseDate
+            planetConnection {
+              totalCount
+              planets {
+                name
+                population
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const Home = () =>
+{
+  const { loading, error, data, fetchMore } = useQuery(GET_LOCATIONS, {
+    variables: { after: null },
+  } );
+  console.log({data})
+
+  if (error) return <div>errors</div>;
+  if ( loading || !data ) return <div>loading</div>;
+
+  return (
+      <>
+        {data?.allPeople.people.map((person: { name: string }) => {
+          return <p style={{ color: 'red' }}>{person.name}</p>;
+        })}
+        <button
+          onClick={() => {
+            const { endCursor } = data.allPeople.pageInfo;
+            fetchMore({
+              variables: { after: endCursor },
+              updateQuery: ( prevResult, { fetchMoreResult } ) =>
+              {
+                // console.log({prevResult, fetchMoreResult})
+                fetchMoreResult.allPeople.people = [
+                  ...prevResult.allPeople.people,
+                  ...fetchMoreResult.allPeople.people,
+                ];
+                return fetchMoreResult;
+              },
+            });
+          }}
+        >
+          more
+        </button>
+      </>
+    );
 };
